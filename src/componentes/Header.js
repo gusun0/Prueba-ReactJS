e399f030d6d5a4ed2a7c6cce957b7d5e9e404d06
gusun0 <fpatan@mail.ru>
2021-02-09 18:28:14 -0600

    Pruebas con react

---
+++ src/componentes/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+
+/*
+function Header({titulo}){
+	return(
+		<Fragment>
+		<h1>{titulo}</h1>
+		</Fragment>
+	)
+}
+*/
+const Header = ({titulo}) => {
+
+	return (
+		<Fragment>
+		<h1>{titulo}</h1>
+		</Fragment>
+	);
+}
+
+
+export default Header;
---
+++ src/componentes/Mensaje.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mensaje = () => {
+
+	return (
+		<p>Agrega una cantidad de plazo a pagar para Cotizar</p>
+	);
+}
+
+
+export default Mensaje;
